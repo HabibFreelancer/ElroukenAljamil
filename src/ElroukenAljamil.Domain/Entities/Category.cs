5d9899f56360f796833153d7e9bdb8a7e76cf0a7
EuropGreenSolar <europgreensolar@gmail.com>
2026-06-03 10:15:44 +0200

    feat: install EF Core, create DbContext, Menu and Category entities

---
+++ src/ElroukenAljamil.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+namespace ElroukenAljamil.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public int MenuId { get; set; }
+    public int? ParentCategoryId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public bool IsLink { get; set; } = true;
+    public int DisplayOrder { get; set; }
+    public bool IsActive { get; set; } = true;
+    public Menu Menu { get; set; } = null!;
+    public Category? ParentCategory { get; set; }
+    public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+}
---
+++ src/ElroukenAljamil.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+using ElroukenAljamil.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElroukenAljamil.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Menu> Menus => Set<Menu>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Annonce> Annonces => Set<Annonce>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Menu>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Slug).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Icon).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Slug).IsRequired().HasMaxLength(200);
+
+            entity.HasOne(e => e.Menu)
+                  .WithMany(m => m.Categories)
+                  .HasForeignKey(e => e.MenuId)
+                  .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(e => e.ParentCategory)
+                  .WithMany(c => c.SubCategories)
+                  .HasForeignKey(e => e.ParentCategoryId)
+                  .OnDelete(DeleteBehavior.Restrict);
+        });
+    }
+}
