8d991cfb67adceeccbc0a35663f73b7783b39363
EuropGreenSolar <europgreensolar@gmail.com>
2026-06-12 17:37:44 +0200

    chore: sync 013 SQL script with current DB state, remove 014 - Merged 014_UpdateMotoDetails into 013_SeedDepositWorkflow_Motos - 34 brands (was 22), model as dependent_select (was text) - motoType as dependent_select (was select) - Added power field (Ch) at order 7 - Added history multiselect (9 options) at order 13 - Reordered: mileage=8, license=9, color=10, vehicleState=11, equipment=12 - Total: 7 steps, 19 fields

--- src/ElroukenAljamil.Infrastructure/Data/Scripts/013_SeedDepositWorkflow_Motos.sql
+++ src/ElroukenAljamil.Infrastructure/Data/Scripts/013_SeedDepositWorkflow_Motos.sql
@@ -35,37 +35,39 @@ BEGIN
     INSERT INTO StepFields (StepId, FieldKey, Label, FieldType, Placeholder, Options, DefaultValue, Suffix, HelperText, IsRequired, DisplayOrder, IsActive, MaxLength, ValidationRegex)
     VALUES (@StepTitle, 'title', N'Titre de l''annonce', 'text', N'Ex: Yamaha MT-07 2022 ABS', '', '', '', '', 1, 1, 1, 200, '');
 
-    -- Step: Details (11 fields)
+    -- Step: Details (13 fields)
     INSERT INTO StepFields (StepId, FieldKey, Label, FieldType, Placeholder, Options, DefaultValue, Suffix, HelperText, IsRequired, DisplayOrder, IsActive, MaxLength, ValidationRegex) VALUES
     (@StepDetails, 'brand', N'Marque', 'select', N'Choisissez',
-     N'[{"value":"yamaha","label":"Yamaha"},{"value":"honda","label":"Honda"},{"value":"bmw","label":"BMW"},{"value":"kawasaki","label":"Kawasaki"},{"value":"suzuki","label":"Suzuki"},{"value":"triumph","label":"Triumph"},{"value":"ducati","label":"Ducati"},{"value":"harley","label":"Harley-Davidson"},{"value":"ktm","label":"KTM"},{"value":"aprilia","label":"Aprilia"},{"value":"mv_agusta","label":"MV Agusta"},{"value":"benelli","label":"Benelli"},{"value":"royal_enfield","label":"Royal Enfield"},{"value":"husqvarna","label":"Husqvarna"},{"value":"indian","label":"Indian"},{"value":"piaggio","label":"Piaggio"},{"value":"vespa","label":"Vespa"},{"value":"sym","label":"SYM"},{"value":"kymco","label":"Kymco"},{"value":"mbk","label":"MBK"},{"value":"peugeot","label":"Peugeot"},{"value":"autre","label":"Autre"}]',
+     N'[{"value":"aprilia","label":"Aprilia"},{"value":"bmw","label":"BMW"},{"value":"benelli","label":"Benelli"},{"value":"bimota","label":"Bimota"},{"value":"bsa","label":"BSA"},{"value":"cagiva","label":"Cagiva"},{"value":"daelim","label":"Daelim"},{"value":"derbi","label":"Derbi"},{"value":"ducati","label":"Ducati"},{"value":"gasgas","label":"GasGas"},{"value":"harley","label":"Harley-Davidson"},{"value":"honda","label":"Honda"},{"value":"husaberg","label":"Husaberg"},{"value":"husqvarna","label":"Husqvarna"},{"value":"hyosung","label":"Hyosung"},{"value":"indian","label":"Indian"},{"value":"kawasaki","label":"Kawasaki"},{"value":"keeway","label":"Keeway"},{"value":"ktm","label":"KTM"},{"value":"kymco","label":"Kymco"},{"value":"mash","label":"Mash"},{"value":"malaguti","label":"Malaguti"},{"value":"mbk","label":"MBK"},{"value":"moto_guzzi","label":"Moto Guzzi"},{"value":"mv_agusta","label":"MV Agusta"},{"value":"peugeot","label":"Peugeot"},{"value":"piaggio","label":"Piaggio"},{"value":"royal_enfield","label":"Royal Enfield"},{"value":"sherco","label":"Sherco"},{"value":"suzuki","label":"Suzuki"},{"value":"sym","label":"SYM"},{"value":"triumph","label":"Triumph"},{"value":"vespa","label":"Vespa"},{"value":"yamaha","label":"Yamaha"},{"value":"autre","label":"Autre"}]',
      '', '', '', 1, 1, 1, NULL, ''),
-    (@StepDetails, 'model', N'Modele', 'text', N'Ex: MT-07, CB650R, R1250GS...', '', '', '', '', 1, 2, 1, 100, ''),
+    (@StepDetails, 'model', N'Modele', 'dependent_select', N'Choisissez', '', '', '', '', 1, 2, 1, 100, ''),
     (@StepDetails, 'year', N'Annee modele', 'select', N'Choisissez',
      N'[{"value":"2024","label":"2024"},{"value":"2023","label":"2023"},{"value":"2022","label":"2022"},{"value":"2021","label":"2021"},{"value":"2020","label":"2020"},{"value":"2019","label":"2019"},{"value":"2018","label":"2018"},{"value":"2017","label":"2017"},{"value":"2016","label":"2016"},{"value":"2015","label":"2015"},{"value":"avant_2015","label":"Avant 2015"}]',
      '', '', '', 1, 3, 1, NULL, ''),
     (@StepDetails, 'vehicleType', N'Type de vehicule', 'select', N'Choisissez',
      N'[{"value":"moto","label":"Moto"},{"value":"scooter","label":"Scooter"},{"value":"quad","label":"Quad"},{"value":"sidecar","label":"Side-car"},{"value":"autre","label":"Autre"}]',
      '', '', '', 0, 4, 1, NULL, ''),
-    (@StepDetails, 'motoType', N'Type de moto', 'select', N'Choisissez',
-     N'[{"value":"sportive","label":"Sportive"},{"value":"roadster","label":"Roadster"},{"value":"trail","label":"Trail / Enduro"},{"value":"custom","label":"Custom / Cruiser"},{"value":"touring","label":"Touring / GT"},{"value":"cafe_racer","label":"Cafe Racer"},{"value":"cross","label":"Cross / Supermotard"},{"value":"classique","label":"Classique / Vintage"},{"value":"utilitaire","label":"Utilitaire"},{"value":"autre","label":"Autre"}]',
-     '', '', '', 0, 5, 1, NULL, ''),
+    (@StepDetails, 'motoType', N'Type de moto', 'dependent_select', N'Choisissez', '', '', '', '', 0, 5, 1, NULL, ''),
     (@StepDetails, 'cylindree', N'Cylindree', 'select', N'Choisissez',
      N'[{"value":"moins_50","label":"Moins de 50 cm3"},{"value":"50_125","label":"50 - 125 cm3"},{"value":"125_600","label":"125 - 600 cm3"},{"value":"600_900","label":"600 - 900 cm3"},{"value":"900_1200","label":"900 - 1200 cm3"},{"value":"plus_1200","label":"Plus de 1200 cm3"}]',
      '', '', '', 0, 6, 1, NULL, ''),
-    (@StepDetails, 'mileage', N'Kilometrage', 'number', N'0', '', '', 'km', '', 1, 7, 1, NULL, ''),
+    (@StepDetails, 'power', N'Puissance', 'number', N'0', '', '', 'Ch', '', 0, 7, 1, NULL, ''),
+    (@StepDetails, 'mileage', N'Kilometrage', 'number', N'0', '', '', 'km', '', 1, 8, 1, NULL, ''),
     (@StepDetails, 'license', N'Type de permis', 'radio', N'',
      N'[{"value":"a","label":"Permis A"},{"value":"a2","label":"Permis A2"},{"value":"a1","label":"Permis A1 (125 cm3)"},{"value":"sans","label":"Sans permis"}]',
-     '', '', '', 0, 8, 1, NULL, ''),
+     '', '', '', 0, 9, 1, NULL, ''),
     (@StepDetails, 'color', N'Couleur', 'select', N'Choisissez',
      N'[{"value":"noir","label":"Noir"},{"value":"blanc","label":"Blanc"},{"value":"gris","label":"Gris"},{"value":"rouge","label":"Rouge"},{"value":"bleu","label":"Bleu"},{"value":"vert","label":"Vert"},{"value":"orange","label":"Orange"},{"value":"jaune","label":"Jaune"},{"value":"autre","label":"Autre"}]',
-     '', '', '', 0, 9, 1, NULL, ''),
+     '', '', '', 0, 10, 1, NULL, ''),
     (@StepDetails, 'vehicleState', N'Etat du vehicule', 'select', N'Choisissez',
      N'[{"value":"neuf","label":"Neuf"},{"value":"tres_bon","label":"Tres bon etat"},{"value":"bon","label":"Bon etat"},{"value":"usure","label":"Traces d''usure normales"},{"value":"accident","label":"Accidente / Pour pieces"}]',
-     '', '', '', 0, 10, 1, NULL, ''),
+     '', '', '', 0, 11, 1, NULL, ''),
     (@StepDetails, 'equipment', N'Equipements', 'multiselect', N'Selectionnez',
      N'[{"value":"abs","label":"ABS"},{"value":"antidemarrage","label":"Antidemarrage"},{"value":"bequille_centrale","label":"Bequille centrale"},{"value":"demarreur_elec","label":"Demarreur electrique"},{"value":"indicateur_rapport","label":"Indicateur de rapport engage"},{"value":"jauge_essence","label":"Jauge a essence"},{"value":"poignees_chauffantes","label":"Poignees chauffantes"},{"value":"topcase","label":"Top case / Valises"},{"value":"alarme","label":"Alarme"},{"value":"carnet_entretien","label":"Carnet d''entretien disponible"}]',
-     '', '', '', 0, 11, 1, NULL, '');
+     '', '', '', 0, 12, 1, NULL, ''),
+    (@StepDetails, 'history', N'Historique et entretien', 'multiselect', N'Selectionnez',
+     N'[{"value":"carnet_jour","label":"Carnet d''entretien a jour"},{"value":"factures","label":"Factures disponibles"},{"value":"premiere_main","label":"Premiere main"},{"value":"non_fumeur","label":"Non fumeur"},{"value":"concession","label":"Toujours entretenue en concession"},{"value":"revisions","label":"Revisions regulieres"},{"value":"aucun_frais","label":"Aucun frais a prevoir"},{"value":"accident","label":"Vehicule accidente / repare"},{"value":"historique_complet","label":"Historique complet disponible"}]',
+     '', '', '', 0, 13, 1, NULL, '');
 
     -- Step: Description
     INSERT INTO StepFields (StepId, FieldKey, Label, FieldType, Placeholder, Options, DefaultValue, Suffix, HelperText, IsRequired, DisplayOrder, IsActive, MaxLength, ValidationRegex) VALUES
@@ -86,7 +88,7 @@ BEGIN
     (@StepContact, 'phone', N'Telephone', 'phone', N'', '', '', '', '', 1, 2, 1, NULL, ''),
     (@StepContact, 'hidePhone', N'Masquer le numero', 'toggle', '', '', 'false', '', '', 0, 3, 1, NULL, '');
 
-    PRINT 'Workflow "Vehicules > Motos" created with 7 steps and 17 fields.';
+    PRINT 'Workflow "Vehicules > Motos" created with 7 steps and 19 fields.';
 END
 ELSE
     PRINT 'Workflow already exists or category not found.';
